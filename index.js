fd922b13259c8e85d284e4aa0ee411a6f8122510
fearless-snippet-5362 <cpk1115@gmail.com>
2019-03-26 18:57:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeli, name) {
-  katzDeli.push(name)
-  return "Welcome, " + name + ". You are number " + katzDeli.length + " in line."
+  katzDeli.push(name);
+  return "Welcome, " + name + ". You are number " + katzDeli.length + " in line.";
 }
 
 function nowServing(katzDeliLine) {
@@ -11,16 +11,16 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var array = []
+  var array = [];
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."}
     
   else {
       for(let i = 0; i < katzDeliLine.length; i++) {
-        array += (i + 0) + ". " + katzDeliLine[i] + ", "
+        array += (i + 1) + ". " + katzDeliLine[i] + ", ";
       }
-      array = array.slice(0, array.length - 2)
-      return "The line is currently: " + array
+      array = array.slice(0, array.length - 2);
+      return "The line is currently: " + array;
     }
 }
 
